c74228860776c2b59fe2b17e95251099f0f1e099
Lenier Ortiz <lenier543@gmail.com>
2023-04-16 18:16:19 -0400

    new version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import image from '@astrojs/image';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-import addClasses from 'rehype-add-classes';
-
-import vercel from "@astrojs/vercel/serverless";
+import image from '@astrojs/image'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config'
+import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,21 +12,26 @@ export default defineConfig({
   integrations: [sitemap(), react(), image(), tailwind()],
   markdown: {
     extendDefaultPlugins: true,
-    rehypePlugins: [[addClasses, {
-      h1: 'text-4xl font-bold font-mplus',
-      h2: 'text-2xl font-bold font-mplus',
-      h3: 'text-xl font-bold font-mplus',
-      h4: 'text-lg font-bold font-mplus',
-      h5: 'font-bold font-mplus',
-      h6: 'font-bold font-mplus',
-      img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
-      p: 'mb-6',
-      a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500',
-      ul: 'max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400',
-      ol: 'list-decimal',
-      li: 'text-orange-500'
-    }]]
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-bold font-mplus',
+          h2: 'text-2xl font-bold font-mplus',
+          h3: 'text-xl font-bold font-mplus',
+          h4: 'text-lg font-bold font-mplus',
+          h5: 'font-bold font-mplus',
+          h6: 'font-bold font-mplus',
+          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6',
+          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500',
+          ul: 'max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400',
+          ol: 'list-decimal',
+          li: 'text-orange-500'
+        }
+      ]
+    ]
   },
-  output: "server",
+  output: 'server',
   adapter: vercel()
-});
+})
